03e0bb7ace9c0300fd0d622d3ab0aa852b65328f
Daniel Uruijo <urquijodn@gmail.com>
2022-09-29 22:14:36 -0500

    Avances: 1- He habilitado WhatsApp y el botón de redes sociales

--- js/app.js
+++ js/app.js
@@ -1,105 +1,105 @@
-const menu                = document.querySelector('.hamburguesa');
-const navegacion          = document.querySelector('.navegacion');
-const imagenes            = document.querySelectorAll('img');
-const btnTodos            = document.querySelector('.todos');
-const btnDesayunos        = document.querySelector('.desayunos');
-const btnTortas            = document.querySelector('.tortas');
-const btnPostres          = document.querySelector('.postres');
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+const imagenes = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.todos');
+const btnDesayunos = document.querySelector('.desayunos');
+const btnTortas = document.querySelector('.tortas');
+const btnPostres = document.querySelector('.postres');
 const contenedorPlatillos = document.querySelector('.platillos');
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     eventos();
     platillos();
 });
 
-const eventos = () =>{
-    menu.addEventListener('click',abrirMenu);
+const eventos = () => {
+    menu.addEventListener('click', abrirMenu);
 }
 
-const abrirMenu = () =>{
-     navegacion.classList.remove('ocultar');
-     botonCerrar();
+const abrirMenu = () => {
+    navegacion.classList.remove('ocultar');
+    botonCerrar();
 }
 
-const botonCerrar = () =>{
+const botonCerrar = () => {
     const btnCerrar = document.createElement('p');
-    const overlay  = document.createElement('div');
+    const overlay = document.createElement('div');
     overlay.classList.add('pantalla-completa');
     const body = document.querySelector('body');
-    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
+    if (document.querySelectorAll('.pantalla-completa').length > 0) return;
     body.appendChild(overlay);
     btnCerrar.textContent = 'x';
     btnCerrar.classList.add('btn-cerrar');
 
-    navegacion.appendChild(btnCerrar);   
-    cerrarMenu(btnCerrar,overlay);
+    navegacion.appendChild(btnCerrar);
+    cerrarMenu(btnCerrar, overlay);
 
 }
 
-const observer = new IntersectionObserver((entries, observer)=>{
-        entries.forEach(entry=>{
-            if(entry.isIntersecting){
-                const imagen = entry.target;
-                imagen.src = imagen.dataset.src;
-                observer.unobserve(imagen);
-            }
-        }); 
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const imagen = entry.target;
+            imagen.src = imagen.dataset.src;
+            observer.unobserve(imagen);
+        }
+    });
 });
 
-imagenes.forEach(imagen=>{
+imagenes.forEach(imagen => {
 
     observer.observe(imagen);
 });
-const cerrarMenu = (boton, overlay) =>{
-    boton.addEventListener('click',()=>{
+const cerrarMenu = (boton, overlay) => {
+    boton.addEventListener('click', () => {
         navegacion.classList.add('ocultar');
         overlay.remove();
         boton.remove();
     });
 
-    overlay.onclick = function(){
+    overlay.onclick = function () {
         overlay.remove();
-        navegacion.classList.add('ocultar');  
+        navegacion.classList.add('ocultar');
         boton.remove();
     }
 }
-const platillos = () =>{
+const platillos = () => {
     let platillosArreglo = [];
     const platillos = document.querySelectorAll('.platillo');
 
-    platillos.forEach(platillo=> platillosArreglo = [...platillosArreglo,platillo]);
+    platillos.forEach(platillo => platillosArreglo = [...platillosArreglo, platillo]);
 
-    const desayunos = platillosArreglo.filter(ensalada=> ensalada.getAttribute('data-platillo') === 'desayuno');
+    const desayunos = platillosArreglo.filter(ensalada => ensalada.getAttribute('data-platillo') === 'desayuno');
     const tortas = platillosArreglo.filter(pasta => pasta.getAttribute('data-platillo') === 'torta');
-    const postres = platillosArreglo.filter(postres=> postres.getAttribute('data-platillo') === 'postres');
+    const postres = platillosArreglo.filter(postres => postres.getAttribute('data-platillo') === 'postres');
 
     mostrarPlatillos(desayunos, tortas, postres, platillosArreglo);
 
 }
 
-const mostrarPlatillos = (desayunos, tortas, postres, todos) =>{
-    btnDesayunos.addEventListener('click', ()=>{
+const mostrarPlatillos = (desayunos, tortas, postres, todos) => {
+    btnDesayunos.addEventListener('click', () => {
         limpiarHtml(contenedorPlatillos);
-        desayunos.forEach(desayunos=> contenedorPlatillos.appendChild(desayunos));
+        desayunos.forEach(desayunos => contenedorPlatillos.appendChild(desayunos));
     });
 
-    btnTortas.addEventListener('click', ()=>{
+    btnTortas.addEventListener('click', () => {
         limpiarHtml(contenedorPlatillos);
-         tortas.forEach(tortas=> contenedorPlatillos.appendChild(tortas));
+        tortas.forEach(tortas => contenedorPlatillos.appendChild(tortas));
     });
 
-    btnPostres.addEventListener('click', ()=>{
+    btnPostres.addEventListener('click', () => {
         limpiarHtml(contenedorPlatillos);
-        postres.forEach(postres=> contenedorPlatillos.appendChild(postres));
+        postres.forEach(postres => contenedorPlatillos.appendChild(postres));
     });
 
-    btnTodos.addEventListener('click',()=>{
+    btnTodos.addEventListener('click', () => {
         limpiarHtml(contenedorPlatillos);
-        todos.forEach(todo=> contenedorPlatillos.appendChild(todo));
+        todos.forEach(todo => contenedorPlatillos.appendChild(todo));
     });
 }
 
-const limpiarHtml = (contenedor) =>{
-    while(contenedor.firstChild){
+const limpiarHtml = (contenedor) => {
+    while (contenedor.firstChild) {
         contenedor.removeChild(contenedor.firstChild);
     }
 }
